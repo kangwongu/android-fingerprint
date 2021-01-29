17c711b1a09a9bc8aa6aba41e5b6e25a7e2628fe
kangwongu <kwg0527@naver.com>
2021-01-29 22:34:23 +0900

    툴바, 네비게이션바 시범..

--- src/main/java/com/example/fingerprint/SystemSettingsActivity.java
+++ src/main/java/com/example/fingerprint/SystemSettingsActivity.java
@@ -3,20 +3,33 @@ package com.example.fingerprint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SystemSettingsActivity extends AppCompatActivity {
 
     // xml의 컴포넌트 연결 위한 변수 선언
     RelativeLayout RelativeLayout_lockapplication, RelativeLayout_fingerprintmethod;
-
+    Toolbar toolbar;
+    ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system_settings);
+
+        // 툴바를 액션바로 설정
+        toolbar = findViewById(R.id.systoolbar);
+        setSupportActionBar(toolbar);
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(false);    // 기존 title 삭제
+        actionBar.setDisplayHomeAsUpEnabled(true);      // 뒤로가기 버튼 생성
+        actionBar.setHomeAsUpIndicator(R.drawable.icon_menu);   // 뒤로가기 버튼 이미지 변경(메뉴)
 
         Intent intent = getIntent();
 
@@ -43,5 +56,16 @@ public class SystemSettingsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home: {
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
